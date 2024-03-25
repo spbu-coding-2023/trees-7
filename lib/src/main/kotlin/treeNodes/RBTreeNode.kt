1133030f6a7eb985d9a93390d9fde25e075cfa08
dronshock <anbrew200212@gmail.com>
2024-03-25 14:59:30 +0300

    feat!: change RBTreeNode class properties
    Make `color` and `parent` variables private
    Add getters and setters for it

--- lib/src/main/kotlin/treeNodes/RBTreeNode.kt
+++ lib/src/main/kotlin/treeNodes/RBTreeNode.kt
@@ -1,6 +1,6 @@
 package treeNodes
 
-public enum class Color {
+enum class Color {
     RED,
     BLACK,
 }
@@ -8,6 +8,21 @@ public enum class Color {
 class RBTreeNode<K : Comparable<K>, V>(
     key: K,
     data: V,
-    var color: Color = Color.BLACK,
-    var parent: RBTreeNode<K, V>? = null,
-) : BinaryTreeNode<K, V, RBTreeNode<K, V>>(key, data)
+    private var color: Color = Color.BLACK,
+    private var parent: RBTreeNode<K, V>? = null,
+) : BinaryTreeNode<K, V, RBTreeNode<K, V>>(key, data) {
+    fun getColor(): Color {
+        return color
+    }
+
+    fun setColor(newColor: Color) {
+        color = newColor
+    }
+    fun getParent(): RBTreeNode<K, V>? {
+        return parent
+    }
+
+    fun setParent(newParent: RBTreeNode<K, V>?) {
+        parent = newParent
+    }
+}
